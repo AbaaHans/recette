f087eca75dc92d536723ef7a21d4e1a257092c00
AbaaHans <brownshans@gmail.com>
2023-09-16 03:14:24 +0100

    register form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -44,7 +44,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column(type:'string')]
     #[Assert\NotBlank()]
-    private ?string $password = null;
+    private ?string $password = 'password';
 
     #[ORM\Column( type:'datetime_immutable')]
     #[Assert\NotNull()]
